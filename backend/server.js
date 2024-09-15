b2f6387077d82f299f1cc56a542f1e35a5983ab9
zishusak <sakzishu016@gmail.com>
2024-09-16 01:48:56 +0400

    Add cors package for connect ui to backend cross-origin pass

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const scheduleRoutes = require('./routes/scheduleRoutes');
+const cors = require('cors');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
